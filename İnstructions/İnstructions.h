552e5380774c3a27c0a569239a2bfbff3063d116
Metehan5433 <leylabicakci01@gmail.com>
2025-11-30 19:50:16 +0300

    instructions.h

---
+++ İnstructions/İnstructions.h
@@ -0,0 +1,15 @@
+// instructions.h
+// Windows 10 Emulator - Instruction Definitions
+
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+
+#include <stdint.h>
+
+// Interrupt handler prototipi
+void handle_interrupt(uint8_t int_num);
+
+// Burada ileride opcode tabloları tanımlanacak
+// Örn: MOV, ADD, SUB, JMP, CALL vs.
+
+#endif // INSTRUCTIONS_H
